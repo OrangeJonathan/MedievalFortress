9dca3f47c7f13e9b08dac350f6ba6a8b5ef919b0
bilnik67 <hoflandbilly@gmail.com>
2024-03-18 15:53:14 +0100

    Revert "Added WaveUI + WaveHandler"
    This reverts commit b21291b7d674785e78d0b55c8d74a7dee89e25fd.

--- MedievalFortress/Assets/Scripts/EnemyMover.cs
+++ MedievalFortress/Assets/Scripts/EnemyMover.cs
@@ -17,40 +17,14 @@ public class EnemyMover : MonoBehaviour
 
     void Update()
     {
-        MoveEnemyToNextWayPoint();
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(speed * Time.deltaTime * direction.normalized, Space.World);
 
-        //Enemy reached target
         if (Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
             GetNextWaypoint();
         }
 
-    }
-
-    void MoveEnemyToNextWayPoint()  
-    {
-        //Move enemy towards target
-        Vector3 direction = target.position - transform.position;
-
-        transform.Translate(speed * Time.deltaTime * direction.normalized, Space.World);
-
-        // Calculate the direction for rotation
-        Vector3 flatDirection = new Vector3(target.position.x - transform.position.x, target.position.y - transform.position.y, target.position.z - transform.position.z);
-        Quaternion flatRotation = Quaternion.LookRotation(flatDirection);
-
-        // Convert to euler angles, and only change the Y rotation
-        Vector3 targetEulerAngles = flatRotation.eulerAngles;
-        float initialXRotation = transform.rotation.eulerAngles.x;
-        float initialZRotation = transform.rotation.eulerAngles.z;
-        targetEulerAngles.x = initialXRotation; 
-        targetEulerAngles.z = initialZRotation; 
-
-        // Convert back to quaternion rotation
-        Quaternion targetRotation = Quaternion.Euler(targetEulerAngles);
-
-        // Rotate towards the target rotation
-        float rotationSpeed = 5f;
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
     void GetNextWaypoint()
