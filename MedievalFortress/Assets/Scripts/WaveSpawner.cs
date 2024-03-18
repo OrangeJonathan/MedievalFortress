b21291b7d674785e78d0b55c8d74a7dee89e25fd
bilnik67 <hoflandbilly@gmail.com>
2024-03-18 15:35:22 +0100

    Added WaveUI + WaveHandler

--- MedievalFortress/Assets/Scripts/WaveSpawner.cs
+++ MedievalFortress/Assets/Scripts/WaveSpawner.cs
@@ -35,6 +35,6 @@ public class WaveSpawner : MonoBehaviour
 
     void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        Instantiate(enemyPrefab, spawnPoint.position, enemyPrefab.rotation);
     }
 }
